ad22d326721bb5d07b2f77667f366eed86762f19
Hyde46 <denis.heid@protonmail.com>
2021-11-11 23:27:33 +0100

    fix get started button

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            href="https://www.github.com/hyde46/hoard#install">
             Get started
           </Link>
         </div>
